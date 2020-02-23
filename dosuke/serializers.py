d1a398ed980152e63a2d4e40fede52b1675ec8f1
たきざわゆうすけ <takky.yunama71@gmail.com>
2020-02-23 11:45:56 +0900

    remove api

--- dosuke/serializers.py
+++
@@ -1,7 +0,0 @@
-from .models import Event
-from rest_framework import serializers
-
-class EventSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Event
-        fields = ('name', 'band_list','mamo')
--- dosuke/urls.py
+++ dosuke/urls.py
@@ -1,25 +1,17 @@
 from django.urls import path
-from django.conf.urls import include, url
-from rest_framework.routers import DefaultRouter
-from django.views.generic import RedirectView
-from .views import EventView, ItemDetailView, ItemCreateView, ItemUpdateView, ItemDeleteView, EventViewSet
-
-router = DefaultRouter()
-router.register(r'Events', EventViewSet)
+from .views import EventView, EventDetailView, ItemCreateView, ItemUpdateView, ItemDeleteView, EventGenerateView
 
 urlpatterns = [
     # 一覧画面
     path('',  EventView.as_view(), name='index'),
+    # 生成結果画面
+    path('generate/<int:pk>/', EventGenerateView.as_view(), name='generate'),
     # 詳細画面
-    path('detail/<int:pk>/', ItemDetailView.as_view(), name='detail'),
+    path('detail/<int:pk>/', EventDetailView.as_view(), name='detail'),
     # 登録画面
     path('create/', ItemCreateView.as_view(), name='create'),
     # 更新画面
     path('update/<int:pk>/', ItemUpdateView.as_view(), name='update'),
     # 削除画面
     path('delete/<int:pk>/', ItemDeleteView.as_view(), name='delete'),
-    # api
-    url(r'^api/', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url('redirect/', RedirectView.as_view(url='/static/html/index.html')),
 ]
