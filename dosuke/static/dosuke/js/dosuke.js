0db28f5bcb05e3dcaca1585c94200e0bcecf3147
たきざわゆうすけ <takky.yunama71@gmail.com>
2020-03-23 07:44:49 +0900

    add comment

--- dosuke/static/dosuke/js/dosuke.js
+++ dosuke/static/dosuke/js/dosuke.js
@@ -18,40 +18,40 @@ $('.form-check-label').mouseenter(function() {
     $(`#${forEle}`).prop('checked', (!isChecked) ? true : false);
   }
 });
-
+// チェックボックスをクリックした時の挙動
 $('.form-check-label').on('mousedown', function(e) {
   const forEle = $(this).attr('for');
   var isChecked = $(`#${forEle}`).prop('checked');
   $(`#${forEle}`).prop('checked', (!isChecked) ? true : false);
 });
-
 $('.form-check-input').on('click', function(e) {
   e.preventDefault();
 });
 
+// バンド内全選択
 $('.select-all').on('click', function(e) {
   const name = $(this).attr('name');
   $(`.${name}`).prop('checked', true);
 });
 
+// バンド内全解除
 $('.non-select-all').on('click', function(e) {
   const name = $(this).attr('name');
   $(`.${name}`).prop('checked', false);
 });
 
+// 一括選択
 $('.global-select-all').on('click', function(e) {
   $(`.form-check-input`).prop('checked', true);
 })
 
+// 一括解除
 $('.global-non-select-all').on('click', function(e) {
   $(`.form-check-input`).prop('checked', false);
 })
 
+// 土スケ生成待機中の表示
 function dispLoading(msg){
-  // 引数なし（メッセージなし）を許容
-  if( msg == undefined ){
-    msg = "";
-  }
   // 画面表示メッセージ
   var dispMsg = "<div class='loadingMsg'>" + msg + "</div>";
   // ローディング画像が表示されていない場合のみ出力
@@ -59,13 +59,13 @@ function dispLoading(msg){
     $("body").append("<div id='loading'>" + dispMsg + "</div>");
   }
 }
-
 $(function () {
   $(".btn-generate").click( function() {
     dispLoading("土スケ生成中...");
   });
 });
 
+// 設定更新アラートの時限非表示
 setTimeout(function(){
   $('.appear-2s').addClass("d-none");
 },2000);
