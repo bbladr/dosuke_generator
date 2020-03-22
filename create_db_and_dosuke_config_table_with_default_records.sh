dc86e3b87056f86d92d3b22044a5052599a1e050
たきざわゆうすけ <takky.yunama71@gmail.com>
2020-03-23 07:10:52 +0900

    fix

--- create_db_and_dosuke_config_table_with_default_records.sh
+++ create_db_and_dosuke_config_table_with_default_records.sh
@@ -1,9 +1,9 @@
 
-echo ".open db_name" | sqlite3
-sqlite3 db_name "CREATE TABLE IF NOT EXISTS "dosuke_config" ("id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "key" varchar(20) NOT NULL, "value" text NOT NULL, "memo" text NULL);"
+echo ".open db.sqlite3" | sqlite3
+sqlite3 db.sqlite3 "CREATE TABLE IF NOT EXISTS "dosuke_config" ("id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "key" varchar(20) NOT NULL, "value" text NOT NULL, "memo" text NULL);"
 
-sqlite3 db_name "insert into dosuke_config values(1,'session_start','14','');"
-sqlite3 db_name "insert into dosuke_config values(2,'session_end','20','');"
-sqlite3 db_name "insert into dosuke_config values(3,'room_start','0','');"
-sqlite3 db_name "insert into dosuke_config values(4,'room_end','27','');"
-sqlite3 db_name "insert into dosuke_config values(5,'max_days','4','');"
+sqlite3 db.sqlite3 "insert into dosuke_config values(1,'session_start','14','');"
+sqlite3 db.sqlite3 "insert into dosuke_config values(2,'session_end','20','');"
+sqlite3 db.sqlite3 "insert into dosuke_config values(3,'room_start','0','');"
+sqlite3 db.sqlite3 "insert into dosuke_config values(4,'room_end','27','');"
+sqlite3 db.sqlite3 "insert into dosuke_config values(5,'max_days','4','');"
